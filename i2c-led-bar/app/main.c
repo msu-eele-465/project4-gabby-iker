1caef2c6336d9fa3d358ca781b6b5bbc342fd959
Gabriella Lord <gabriellalord@icloud.com>
2025-03-24 16:10:03 -0600

    Modified i2c-slave-led test files. Slave should meet spec, untested

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -32,9 +32,37 @@ unsigned int timing_base = 32768;
 int timing_adj;
 unsigned char ledPattern_state;
 
+//------------------------------------------------------------------------------
+// Begin I2C initialization
+//------------------------------------------------------------------------------
+#define SLAVE_ADDR  0x68                    // Slave I2C Address
+volatile unsigned char receivedData = 0;    // Recieved data
+
+void I2C_Slave_Init(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;  // Stop Watchdog Timer
+
+    // Configure P1.2 as SDA and P1.3 as SCL
+    P1SEL1 &= ~(BIT2 | BIT3);
+    P1SEL0 |= BIT2 | BIT3;
+
+    // Configure USCI_B0 as I2C Slave
+    UCB0CTLW0 |= UCSWRST;               // Put eUSCI_B0 into software reset
+    UCB0CTLW0 |= UCMODE_3;              // Select I2C slave mode
+    UCB0I2COA0 = SLAVE_ADDR + UCOAEN;   // Set and enable first own address
+    UCB0CTLW0 |= UCTXACK;               // Send ACKs
+
+    PM5CTL0 &= ~LOCKLPM5;               // Disable low-power inhibit mode
+
+    UCB0CTLW0 &= ~UCSWRST;              // Pull eUSCI_B0 out of software reset
+    UCB0IE |= UCSTTIE + UCRXIE;         // Enable Start and RX interrupts
+
+    __enable_interrupt();               // Enable Maskable IRQs
+}
+//--End I2C initializatoin------------------------------------------------------
 
 //------------------------------------------------------------------------------
-// Begin led initialization
+// Begin LED initialization
 //------------------------------------------------------------------------------
 void init_led_bar()
 {
@@ -205,20 +233,28 @@ void set_led_bar(char key_input)
     }
 }
 
-void main(void)
+int main(void)
 {
     init_led_bar();
-    while(true){}
+    I2C_Slave_Init();                   // Initialize the slave for I2C
+    __bis_SR_register(LPM0_bits + GIE); // Enter LPM0, enable interrupts
+    return 0;
 }
 
 
 //------------------------------------------------------------------------------
 // Begin Interrupt Service Routines
 //------------------------------------------------------------------------------
-#pragma vector = TIMER1_B0_VECTOR
-__interrupt void ISR_TB1_CCR0(void)
+#pragma vector = USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void)
 {
-    led_patterns('5');
-    TB1CCTL0 &= ~CCIFG;
+    switch (__even_in_range(UCB0IV, USCI_I2C_UCTXIFG0))
+    {
+        case USCI_I2C_UCRXIFG0:         // Receive Interrupt
+            led_patterns(UCB0RXBUF);    // Read received data
+            break;
+        default: 
+            break;
+    }
 }
 //-- End Interrupt Service Routines --------------------------------------------
--- i2c-led-bar/test/slave_test_led.c
+++ i2c-led-bar/test/slave_test_led.c
@@ -1,54 +1,53 @@
 #include "intrinsics.h"
-#include "msp430fr2355.h"
 #include <msp430.h>
 #include <stdbool.h>
-#include "led_bar_slave.h"
+
+#define ledOn 0xFF
+#define ledOff 0
+#define ledPattern01_init 0b10101010
+#define ledPattern02_init 0
+#define ledPattern03_init 0b00011000
+#define ledPattern04_init 0xFF
+#define ledPattern05_init 0b00000001
+#define ledPattern06_init 0b01111111
+#define ledPattern07_init 0b00000001
 
 bool new_input_bool = true;
 bool pattern3_out = true;
 char key_cur;
 char key_prev = '\0';
-char key_temp;
 int pattern1_cur;
 int pattern2_cur;
 int pattern3_cur;
 int pattern4_cur;
 int pattern5_cur;
 int pattern6_cur;
-int pattern7_cur;
 bool pattern1_start = false;
 bool pattern2_start = false;
 bool pattern3_start = false;
 bool pattern4_start = false;
 bool pattern5_start = false;
 bool pattern6_start = false;
-bool pattern7_start = false;
 unsigned int timing_base = 32768;
 int timing_adj;
+unsigned char ledPattern_state;
+
 
 //------------------------------------------------------------------------------
 // Begin led initialization
 //------------------------------------------------------------------------------
 void init_led_bar()
 {
-    #define ledOn 0xFF
-    #define ledOff 0
-    #define ledPattern01_init 0b10101010
-    #define ledPattern02_init 0
-    #define ledPattern03_init 0b00011000
-    #define ledPattern04_init 0xFF
-    #define ledPattern05_init 0b00000001
-    #define ledPattern06_init 0b01111111
-    #define ledPattern07_init 0b00000001
-    #define PLEDDIR
-    #define PLEDROUT
+    WDTCTL = WDTPW | WDTHOLD;  // Stop Watchdog Timer
 
     // Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
     
     // Setup Ports
-    P3OUT &= ~0xFF;             // Clear  Pin 3 to start
-    P3DIR |= 0xFF;              // Config Pin 3 as output
+    P1OUT &= ~0xFF;             // Clear  Pin 3 to start
+    P1DIR |= 0xFF;              // Config Pin 3 as output
+    P2OUT &= ~0xFF;             // Clear  Pin 4 to start
+    P2DIR |= 0xFF;              // Config Pin 4 as output
 
     // Setup Timer
     TB1CTL |= TBCLR;            // Clear timers and dividers
@@ -69,112 +68,123 @@ void init_led_bar()
 //--End LED Initialization------------------------------------------------------
 
 //------------------------------------------------------------------------------
-// Begin LED patterns
+// Begin Display LED Patterns
+//------------------------------------------------------------------------------
+void display_led_pattern()
+{
+    P1OUT = ledPattern_state;
+    P2OUT = ledPattern_state << 4 | ledPattern_state >> 7;
+}
+//--End Display LED Patterns----------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Begin LED Patterns
 //------------------------------------------------------------------------------
 void led_patterns(char key_cur) 
 {
     switch(key_cur)
     {
         case '0':           // Static state
-            P3OUT = ledPattern01_init;
+            ledPattern_state = ledPattern01_init;
             break;
         case '1':           // Toggle
             TB1CCR0 = timing_base;
-            if (new_input_bool == true) {
+            if (new_input_bool) {
                 if ((key_cur == key_prev | pattern1_start == false))
                 {
-                    P3OUT = ledPattern01_init;
+                    ledPattern_state = ledPattern01_init;
                     pattern1_start = true;
                 }
                 else
-                    P3OUT = pattern1_cur;
+                    ledPattern_state = pattern1_cur;
                 new_input_bool = false;
             } else
-                pattern1_cur = (P3OUT ^= 0xFF);
+                ledPattern_state = pattern1_cur = (ledPattern_state ^= 0xFF);
             break;
         case '2':             // Up counter
             TB1CCR0 = 0.5 * timing_base;
-            if (new_input_bool == true) {
+            if (new_input_bool) {
                 if (key_cur == key_prev | pattern2_start == false)
                 {
-                    P3OUT = ledPattern02_init;
+                    ledPattern_state = ledPattern02_init;
                     pattern2_start = true;
                 }
                 else
-                    P3OUT = pattern2_cur;
+                    ledPattern_state = pattern2_cur;
                 new_input_bool = false;
             } else
-                pattern2_cur = (P3OUT++);
+                ledPattern_state = pattern2_cur = (++ledPattern_state);
             break;
         case '3':             // in and out
             TB1CCR0 = 0.5 * timing_base;
-            if (new_input_bool == true) {
+            if (new_input_bool) {
                 if (key_cur == key_prev | pattern3_start == false)
                 {
-                    P3OUT = ledPattern03_init;
+                    ledPattern_state = ledPattern03_init;
                     pattern3_start = true;
                 }
                 else
-                    P3OUT = pattern3_cur;
+                    ledPattern_state = pattern3_cur;
                 new_input_bool = false;
             }
-            else if ((pattern3_out == true & P3OUT != 0b10000001) | (pattern3_out == false & P3OUT == 0b00011000))  // out
+            else if ((pattern3_out == true & ledPattern_state != 0b10000001) | (pattern3_out == false & ledPattern_state == 0b00011000))  // out
             {
-                pattern3_cur = (P3OUT = ~P3OUT & ((0xF0 & P3OUT << 1) | (0xF & P3OUT >> 1) | P3OUT << 7 | P3OUT >> 7));
+                ledPattern_state = pattern3_cur = (ledPattern_state = ~ledPattern_state & ((0xF0 & ledPattern_state << 1) | (0xF & ledPattern_state >> 1) | ledPattern_state << 7 | ledPattern_state >> 7));
                 pattern3_out = true;
             }
-            else if ((pattern3_out == false & P3OUT != 0b00011000) | (pattern3_out == true & P3OUT == 0b10000001))  // in
+            else if ((pattern3_out == false & ledPattern_state != 0b00011000) | (pattern3_out == true & ledPattern_state == 0b10000001))  // in
             {
-                pattern3_cur = (P3OUT = ~P3OUT & ((0xF & P3OUT << 1) | (0xF0 & P3OUT >> 1) | P3OUT << 7 | P3OUT >> 7));
+                ledPattern_state = pattern3_cur = (ledPattern_state = ~ledPattern_state & ((0xF & ledPattern_state << 1) | (0xF0 & ledPattern_state >> 1) | ledPattern_state << 7 | ledPattern_state >> 7));
                 pattern3_out = false;
             }
             break;
         case '4':             // down counter, extra credit
             TB1CCR0 = 0.25 * timing_base;
-            if (new_input_bool == true) {
+            if (new_input_bool) {
                 if (key_cur == key_prev | pattern4_start == false)
                 {
-                    P3OUT = ledPattern04_init;
+                    ledPattern_state = ledPattern04_init;
                     pattern4_start = true;
                 }
                 else
-                    P3OUT = pattern4_cur;
+                    ledPattern_state = pattern4_cur;
                 new_input_bool = false;
             } else
-                pattern4_cur = (P3OUT--);
+                ledPattern_state = pattern4_cur = (--ledPattern_state);
             break;
         case '5':             // rotate one left, extra credit
             TB1CCR0 = 1.5 * timing_base;
-        if (new_input_bool == true) {
+        if (new_input_bool) {
             if (key_cur == key_prev | pattern5_start == false)
                 {
-                    P3OUT = ledPattern05_init;
+                    ledPattern_state = ledPattern05_init;
                     pattern5_start = true;
                 }
             else
-                P3OUT = pattern5_cur;
+                ledPattern_state = pattern5_cur;
                 new_input_bool = false;
             } else
-                pattern5_cur = (P3OUT = P3OUT << 1 | P3OUT >> 7);
+                ledPattern_state = pattern5_cur = (ledPattern_state = ledPattern_state << 1 | ledPattern_state >> 7);
             break;
         case '6':             // rotate 7 right, extra credit
             TB1CCR0 = 0.5 * timing_base;
-            if (new_input_bool == true) {
+            if (new_input_bool) {
                 if (key_cur == key_prev | pattern6_start == false)
                 {
-                    P3OUT = ledPattern06_init;
+                    ledPattern_state = ledPattern06_init;
                     pattern6_start = true;
                 }
                 else
-                    P3OUT = pattern6_cur;
+                    ledPattern_state = pattern6_cur;
                 new_input_bool = false;
             } else
-                pattern6_cur = (P3OUT = P3OUT >> 1 | P3OUT << 7);
+                ledPattern_state = pattern6_cur = (ledPattern_state = ledPattern_state >> 1 | ledPattern_state << 7);
             break;
         default:
-            P3OUT = ledOff;
+            ledPattern_state = ledOff;
             break;
     }
+    display_led_pattern();
 }
 
 void set_led_bar(char key_input)
@@ -195,6 +205,12 @@ void set_led_bar(char key_input)
     }
 }
 
+void main(void)
+{
+    init_led_bar();
+    while(true){}
+}
+
 
 //------------------------------------------------------------------------------
 // Begin Interrupt Service Routines
@@ -202,7 +218,7 @@ void set_led_bar(char key_input)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void ISR_TB1_CCR0(void)
 {
-    led_patterns(key_cur);
+    led_patterns('5');
     TB1CCTL0 &= ~CCIFG;
 }
 //-- End Interrupt Service Routines --------------------------------------------
